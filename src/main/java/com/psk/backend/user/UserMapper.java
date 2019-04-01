e12e230ad6eb70fa3ed802921daa8f7b08bd66eb
mJev <ma.prokofjev@gmail.com>
2019-04-01 13:28:14 +0300

    fix docker

--- src/main/java/com/psk/backend/user/UserMapper.java
+++ src/main/java/com/psk/backend/user/UserMapper.java
@@ -4,14 +4,10 @@ import com.psk.backend.config.BaseMapperConfig;
 import com.psk.backend.user.value.NewUserForm;
 import com.psk.backend.user.value.UserListView;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(config = BaseMapperConfig.class)
 public abstract class UserMapper {
 
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "password", ignore = true)
-    @Mapping(target = "status", defaultValue = "CONFIRMATION_PENDING")
     public abstract User create(NewUserForm form);
 
     public abstract UserListView listView(User user);

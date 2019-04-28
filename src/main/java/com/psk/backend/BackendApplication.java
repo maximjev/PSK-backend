b076dde83b5e2cce29f3d4956603b699f9948981
mJev <ma.prokofjev@gmail.com>
2019-04-28 17:45:06 +0300

    add ec2 scripting

--- src/main/java/com/psk/backend/BackendApplication.java
+++ src/main/java/com/psk/backend/BackendApplication.java
@@ -5,14 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
-    private static final String SPRING_ACTIVE_PROFILES = "spring.profiles.active";
-    private static final String PROD_PROFILE = "prod";
 
     public static void main(String[] args) {
-        if (System.getProperty(SPRING_ACTIVE_PROFILES) == null) {
-            System.setProperty(SPRING_ACTIVE_PROFILES, PROD_PROFILE);
-        }
-
         SpringApplication.run(BackendApplication.class, args);
     }
+
 }

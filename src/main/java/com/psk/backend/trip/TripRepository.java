8bd8caf2941fd9af5faa6bac8405fdfe4320a990
mJev <ma.prokofjev@gmail.com>
2019-05-23 22:35:07 +0300

    fix swagger api

--- src/main/java/com/psk/backend/trip/TripRepository.java
+++ src/main/java/com/psk/backend/trip/TripRepository.java
@@ -40,7 +40,7 @@ public class TripRepository {
 
         var entities = mongoOperations.find(
                 query(conditions)
-                        .skip(page.getOffset())
+                        .skip(page.getPageSize() * page.getPageNumber())
                         .limit(page.getPageSize()),
                 Trip.class)
                 .stream()

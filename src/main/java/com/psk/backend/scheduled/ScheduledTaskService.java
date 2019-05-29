74bfcd08f61967514d611cd6671badcdc080ae3e
mJev <ma.prokofjev@gmail.com>
2019-05-29 16:56:11 +0300

    fix scheduled task for trips without reservation

--- src/main/java/com/psk/backend/scheduled/ScheduledTaskService.java
+++ src/main/java/com/psk/backend/scheduled/ScheduledTaskService.java
@@ -8,32 +8,33 @@ import com.psk.backend.trip.TripUserStatus;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
+
+import static java.time.LocalDateTime.now;
 
 @Service
 public class ScheduledTaskService {
     private TripRepository tripRepository;
 
-    public ScheduledTaskService(TripRepository tripRepository){
-        this.tripRepository=tripRepository;
+    public ScheduledTaskService(TripRepository tripRepository) {
+        this.tripRepository = tripRepository;
     }
 
     @Scheduled(fixedRate = 3600)
-    public void updateTripStatus(){
+    public void updateTripStatus() {
         checkStartedTrips();
         checkConfirmedTrips();
         checkDraftedTrips();
     }
 
-    private void checkDraftedTrips(){
-        List <Trip>  trips = tripRepository.getTripsByStatus(TripStatus.DRAFT);
-        for (Trip trip :trips) {
+    private void checkDraftedTrips() {
+        List<Trip> trips = tripRepository.getTripsByStatus(TripStatus.DRAFT);
+        for (Trip trip : trips) {
 
             long noOfPendingUsers = trip.getUsers().stream()
                     .filter(user -> user.getStatus().equals(TripUserStatus.CONFIRMATION_PENDING))
                     .count();
-            if (noOfPendingUsers > 0 && trip.getDeparture().isBefore(LocalDateTime.now())){
+            if (noOfPendingUsers > 0 && trip.getDeparture().isBefore(now())) {
                 trip.setStatus(TripStatus.CANCELLED);
                 tripRepository.save(trip);
                 continue;
@@ -42,7 +43,7 @@ public class ScheduledTaskService {
             long noOfConfirmedUsers = trip.getUsers().stream()
                     .filter(user -> user.getStatus().equals(TripUserStatus.CONFIRMED))
                     .count();
-            if (noOfConfirmedUsers > 0 && noOfPendingUsers==0){
+            if (noOfConfirmedUsers > 0 && noOfPendingUsers == 0) {
                 trip.setStatus(TripStatus.CONFIRMED);
                 tripRepository.save(trip);
                 continue;
@@ -52,7 +53,7 @@ public class ScheduledTaskService {
                     .filter(user -> user.getStatus().equals(TripUserStatus.DECLINED))
                     .count();
             if (noOfDeclinedUsers == trip.getUsers().size()
-                    || trip.getDeparture().isBefore(LocalDateTime.now()) ){
+                    || trip.getDeparture().isBefore(now())) {
                 trip.setStatus(TripStatus.CANCELLED);
                 tripRepository.save(trip);
                 continue;
@@ -60,23 +61,29 @@ public class ScheduledTaskService {
         }
     }
 
-    private void checkConfirmedTrips(){
-        List <Trip>  trips = tripRepository.getTripsByStatus(TripStatus.CONFIRMED);
-        for (Trip trip :trips) {
-            if (trip.getDeparture().isBefore(LocalDateTime.now())) {
+    private void checkConfirmedTrips() {
+        List<Trip> trips = tripRepository.getTripsByStatus(TripStatus.CONFIRMED);
+        for (Trip trip : trips) {
+            if (trip.getDeparture().isBefore(now())) {
                 trip.setStatus(TripStatus.STARTED);
                 tripRepository.save(trip);
             }
         }
     }
 
-    private void checkStartedTrips(){
-        List <Trip>  trips = tripRepository.getTripsByStatus(TripStatus.STARTED);
-        for (Trip trip :trips) {
-            if (trip.getReservationEnd().isBefore(LocalDateTime.now())){
-                trip.setStatus(TripStatus.FINISHED);
-                tripRepository.save(trip);
+    private void checkStartedTrips() {
+        List<Trip> trips = tripRepository.getTripsByStatus(TripStatus.STARTED);
+        for (Trip trip : trips) {
+            if (trip.isReservation()) {
+                if (trip.getReservationEnd().isBefore(now())) {
+                    trip.setStatus(TripStatus.FINISHED);
+                }
+            } else {
+                if (trip.getArrival().isBefore(now())) {
+                    trip.setStatus(TripStatus.FINISHED);
+                }
             }
+            tripRepository.save(trip);
         }
     }
 }

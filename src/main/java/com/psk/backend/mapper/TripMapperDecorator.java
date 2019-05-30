4f812959813a99336b6a86fd8b46ce38822ac6da
mJev <ma.prokofjev@gmail.com>
2019-05-31 01:46:17 +0300

    fix decorator

--- src/main/java/com/psk/backend/mapper/TripMapperDecorator.java
+++ src/main/java/com/psk/backend/mapper/TripMapperDecorator.java
@@ -2,12 +2,18 @@ package com.psk.backend.mapper;
 
 import com.psk.backend.domain.trip.Trip;
 import com.psk.backend.domain.trip.value.TripUserView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public abstract class TripMapperDecorator extends TripMapper {
 
+    @Autowired
+    @Qualifier("delegate")
+    private TripMapper delegate;
+
     @Override
     public TripUserView tripUserView(Trip trip, String userId) {
-        var userView = tripUserView(trip, userId);
+        var userView = delegate.tripUserView(trip, userId);
         var user = trip.getUsers().stream().filter(u -> u.getId().equals(userId)).findFirst();
         return user.map(u -> {
             userView.setCarRent(u.getCarRent());
